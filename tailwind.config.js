bd6dad772467a5d586d5dc260d839ab602b47e54
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-03-07 16:36:21 -0800

    ✨ Add Login Page
    Add a login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,28 @@ module.exports = {
     },
     purge: {
         content: ["./src/**/*.js", "./src/**/**/*.js"]
+    },
+    theme: {
+        fill: (theme) => ({
+            red: theme("colors.red.primary")
+        }),
+        colors: {
+            white: "#ffffff",
+            blue: {
+                medium: "#005c98"
+            },
+            black: {
+                light: "#262626",
+                faded: "#00000059"
+            },
+            gray: {
+                base: "#616161",
+                background: "#fafafa",
+                primary: "#dbdbdb"
+            },
+            red: {
+                primary: "#ed4956"
+            }
+        }
     }
 };
