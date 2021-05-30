a699ceccb11a9fe0b03dd20c03a9ac64ca07ecc7
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-05-29 18:34:14 -0700

    ✨ Add Basic Dashboard Page and Components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import * as Routes from "./constants/routes";
 const Login = lazy(() => import("./pages/login"));
 const Signup = lazy(() => import("./pages/signup"));
 const NotFound = lazy(() => import("./pages/notfound"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
 
 export default function App() {
     return (
         <Router>
             <Suspense fallback={<p>Loading...</p>}>
                 <Switch>
+                    <Route path={Routes.DASHBOARD} component={Dashboard} />
                     <Route path={Routes.LOGIN} component={Login} />
                     <Route path={Routes.SIGN_UP} component={Signup} />
                     <Route component={NotFound} />
