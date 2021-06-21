45c4d41a079a1740ce2759c2bbab8ff918beaa10
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2021-06-21 13:16:44 -0700

    🔧 Update Firebase Config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import "firebase/firestore";
 // import { seedDatabase } from "../seed";
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 const firebase = Firebase.initializeApp(config);
